9a0e822f466691872e62e84df9d1c69d9bfa1f3c
14750482-tech <14750482@me.mcu.edu.tw>
2026-09-17 11:47:38 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,13 @@
+///week02-1.cpp 今天的主題c++ string 字串
+///File-Save as 要自己輸入檔名.cpp
+#include<iostream>///上週交的
+#include<string>///02周交的
+///現在識字多的版本
+int main()
+{
+
+    std::cout<<"請問你叫甚麼名字";
+    std::string name;///宣告字串name
+    std::cin>>name;///上週交的cin原來長這樣
+    std::cout<<name<<"你好，今天教字串";
+}
